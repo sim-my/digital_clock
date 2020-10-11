8ecc4b6daec0103dcf4b2bea45a63fd54f443d34
sim-my <simranpanthi101@gmail.com>
2020-10-11 22:06:54 +0545

    Added comments

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function updateTime() {
-  var days  = new Array(7);  
+  //create new date object
+  var date = new Date();
+
+  //get day
+  var days = new Array(7);
   days[0] = "Sun";
   days[1] = "Mon";
   days[2] = "Tue";
@@ -7,6 +11,9 @@ function updateTime() {
   days[4] = "Thu";
   days[5] = "Fri";
   days[6] = "Sat";
+  var day = days[date.getDay()];
+
+  //get month
   var months = new Array(12);
   months[0] = "Jan";
   months[1] = "Feb";
@@ -20,30 +27,51 @@ function updateTime() {
   months[9] = "Oct";
   months[10] = "Nov";
   months[11] = "Dec";
-  var date = new Date();
-  var hour = date.getHours();
-  var meridian = hour >= 12 ? "pm" : "am";
-  hour = hour % 12;
-  var min = date.getMinutes();
-  var sec = date.getSeconds();
-  var day = days[date.getDay()];
-  var date_no = date.getUTCDate()
   var month = months[date.getMonth()];
-  var year  = date.getFullYear();
+
+  //get hours
+  var original_hour = date.getHours();
+
+  //get 12 format hours
+  var hour = original_hour % 12;
+
+  //get am/pm
+  var meridian = original_hour >= 12 ? "pm" : "am";
+
+  //get minutes
+  var min = date.getMinutes();
+
+  //get seconds
+  var sec = date.getSeconds();
+
+  //get date
+  var date_no = date.getUTCDate();
+
+  //get year
+  var year = date.getFullYear();
+
+  //adding 0 before single digit
   min = min < 10 ? "0" + min : min;
   hour = hour < 10 ? "0" + hour : hour;
   sec = sec < 10 ? "0" + sec : sec;
+
   setTime(hour, min, sec, meridian, day, date_no, month, year);
 }
 
-function setTime(hour, min, sec, meridian, day, date_no, month, year){
-    document.getElementById('meridian').innerHTML = meridian;
-    document.getElementById('hour').innerHTML = hour;
-    document.getElementById('min').innerHTML = min;
-    document.getElementById('sec').innerHTML = sec;
-    document.getElementById('day').innerHTML = day;
-    document.getElementById('date').innerHTML = date_no + ' ' + month;
-    document.getElementById('year').innerHTML = year;
+function setTime(hour, min, sec, meridian, day, date_no, month, year) {
+  document.getElementById("meridian").innerHTML = meridian;
+  document.getElementById("hour").innerHTML = hour;
+  document.getElementById("min").innerHTML = min;
+  document.getElementById("sec").innerHTML = sec;
+  document.getElementById("day").innerHTML = day;
+  document.getElementById("date").innerHTML = date_no + " " + month;
+  document.getElementById("year").innerHTML = year;
 }
 
-window.onload = setInterval(function(){ updateTime(); }, 1000);
+//call update time function
+window.onload = function () {
+  updateTime();
+  setInterval(function () {
+    updateTime();
+  }, 1000);
+};
